7a9a867a8e09ef698aa7d8f54f1d0a3c0c6322e9
unknown <woodong9412@gmail.com>
2021-09-07 18:14:02 +0900

    학습

---
+++ java-source/exam02/src/chapter13_1/OuterClass3Test.java
@@ -0,0 +1,9 @@
+package chapter13_1;
+
+public class OuterClass3Test {
+	public static void main(String[]args) {
+		OuterClass3 out = new OuterClass3();
+		//InnerMyClass in = out.outerMethod();
+		//in.method();
+	}
+}
---
+++ java-source/exam02/src/chapter13_3/StreamEx1.java
@@ -0,0 +1,23 @@
+package chapter13_3;
+
+import java.util.stream.Stream;
+import java.util.Arrays;
+
+public class StreamEx1 {
+	public static void main(String[]args) {
+		Integer[] nums = {0,1,2,3,4,5,6};
+		
+		Stream<Integer> stream = Arrays.stream(nums);
+		//stream.map(x -> x * x).forEacch(x -> System.out.println(x));
+		//long count = stream.map(x -> x * x).count();
+		//System.out.println(count);
+		//int sum = stream.mapToInt(x -> x * x).sum();
+		//System.out.prinltn(sum);
+		
+		int sum = stream.filter(x -> x < 4).mapToInt(x -> x).sum();
+		System.out.println(sum);
+		
+		int sum2 = Arrays.stream(nums).mapToInt(x -> x).sum();
+		System.out.println(sum2);
+	}
+}
